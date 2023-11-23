3592c63f1b76e8879f1dd8d405f664e90ba7bb57
0Leonel <leonel20112010@gmail.com>
2023-11-22 21:42:54 -0300

    env

--- src/credenciales.js
+++ src/credenciales.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCdVOePedink2O4Efuzt92wj9pM2OTQrDs",
-  authDomain: "reactargentina-d7c74.firebaseapp.com",
-  projectId: "reactargentina-d7c74",
-  storageBucket: "reactargentina-d7c74.appspot.com",
-  messagingSenderId: "715367381701",
-  appId: "1:715367381701:web:b6eed015a0d5ef12ed4eac"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID 
 };
 
 // Initialize Firebase
