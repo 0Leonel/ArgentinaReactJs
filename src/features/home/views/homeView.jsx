ff424b0db9d8600cb5dc1f46780f61dd0fbf0cc2
0Leonel <leonel20112010@gmail.com>
2023-11-23 12:55:47 -0300

    prueba

--- src/features/home/views/homeView.jsx
+++ src/features/home/views/homeView.jsx
@@ -6,7 +6,7 @@ const auth = getAuth(appFirebase);
 export const HomeView = (props) => {
   return (
     <div>
-      <h1>Bienvenido {props.users}! <button onClick={() => signOut(auth)} className='bg-blue-500 rounded-lg text-white p-1'>Logout</button></h1>
+      <h1>Welcome {props.users}! <button onClick={() => signOut(auth)} className='bg-blue-500 rounded-lg text-white p-1'>Logout</button></h1>
     </div>
   )
 }
